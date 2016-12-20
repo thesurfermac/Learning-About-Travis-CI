dbcc6ae894f75f21f5da984ca5a7e53be1755cf4
abhinav <mac.abhinav@gmail.com>
2016-12-21 00:24:30 +0530

    changed the text to see if negative values pass the test

--- app_test.py
+++ app_test.py
@@ -27,9 +27,9 @@ class FlaskTestCase(unittest.TestCase):
     # We are checking that 2+6 is 10
     def test_sum_fail(self):
         tester = app.test_client(self)
-        response = tester.get('/_add_numbers?a=2&b=6', content_type='application/json')
+        response = tester.get('/_add_numbers?a=-2&b=-6', content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.data), {"result": 10})
+        self.assertEqual(json.loads(response.data), {"result": -8})
 
 if __name__ == '__main__':
     unittest.main()
